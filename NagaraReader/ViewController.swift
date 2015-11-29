54008da60d5d1214f6ed58d62fa4fd8b89663a0c
a.harada <redhornet96@gmail.com>
2015-11-29 10:15:00 +0900

    init

--- NagaraReader/ViewController.swift
+++ NagaraReader/ViewController.swift
@@ -1,25 +1,90 @@
-//
-//  ViewController.swift
-//  NagaraReader
-//
-//  Created by HaradaAtsushi on 2015/11/29.
-//  Copyright © 2015年 mofmof inc. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MEMELibDelegate, UISearchBarDelegate, UIWebViewDelegate {
+    
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var blinkTimes : [NSDate] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        MEMELib.sharedInstance().delegate = self
+        searchBar.delegate = self
+        webView.delegate = self
+        loadAddressURL()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func memeAppAuthorized(status: MEMEStatus) {
+        MEMELib.sharedInstance().startScanningPeripherals()
+    }
+    
+    func memePeripheralFound(peripheral: CBPeripheral!, withDeviceAddress address: String!) {
+        MEMELib.sharedInstance().connectPeripheral(peripheral)
+    }
+    
+    func memePeripheralConnected(peripheral: CBPeripheral!) {
+        let status = MEMELib.sharedInstance().startDataReport()
+        print(status)
+    }
+    
+    func memeRealTimeModeDataReceived(data: MEMERealTimeData!) {
+        if data.blinkStrength > 0 {
+            blinkTimes.append(NSDate())
+            
+            if blinkTimes.count > 3 {
+                blinkTimes.removeFirst()
+            }
+            
+            // 2秒間以内に3回のまばたきをした場合
+            if blinkTimes.count == 3 {
+                let diffTime = blinkTimes.last!.timeIntervalSinceDate(blinkTimes.first!)
+                if diffTime < 2 {
+                    print(diffTime)
+                    blinkTimes = []
+                    
+                    // 一番下へスクロール
+                    //webView.scrollView.setContentOffset(CGPointMake(0, webView.scrollView.contentSize.height - webView.scrollView.frame.size.height), animated: true)
+                    
+                    // いい感じの位置へスクロール
+                    let currentY = webView.scrollView.contentOffset.y
+                    webView.scrollView.setContentOffset(CGPointMake(0, currentY + webView.scrollView.frame.size.height - 200), animated: true)
+                }
+            }
+        }
+    }
+    
+    func loadAddressURL() {
+        let requestURL = NSURL(string: searchBar.text!)
+        print("requestURL: " + String(requestURL))
+        let req = NSURLRequest(URL: requestURL!)
+        webView.loadRequest(req)
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        searchBar.text = webView.stringByEvaluatingJavaScriptFromString("document.URL")
+    }
+    
+    func searchBarSearchButtonClicked( searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        loadAddressURL()
+    }
+    
+    @IBAction func back(sender: AnyObject) {
+        webView.goBack()
+    }
+    
+    @IBAction func forward(sender: AnyObject) {
+        webView.goForward()
+    }
+    
 }
-
